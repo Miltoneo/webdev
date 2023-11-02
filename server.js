1ac6fe98aa55989059f2de9cfbeed559f7cdc6c7
Miltoneo <138482468+Miltoneo@users.noreply.github.com>
2023-11-02 13:20:42 -0300

    parte 2

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
 if (process.env.NODE_ENV !== 'production') {
   //require('dotenv').load()
-  //require('dotenv').parse()
   require('dotenv').config()  
 }
 
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
@@ -23,6 +25,6 @@ db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
-app.listen(process.env.PORT || process.env.APP_PORT)
-console.log("server running at port: ", process.env.APP_PORT)
+app.listen(process.env.PORT || 3000)
